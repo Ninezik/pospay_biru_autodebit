265f5fdb98fba1cc6a155a17d9fae5e5434522e1
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-05-19 09:54:11 +0700

    Update denom_product_not_specific.sql

--- denom_product_not_specific.sql
+++ denom_product_not_specific.sql
@@ -1,7 +1,8 @@
 SELECT distinct 
 transaction_amount,
 dst_account_type,
-dst_bank_code
+dst_bank_code,
+payee_code
 FROM public.t_transaction_history_scheduler
 where dst_account_owner_name='Pulsa Telkomsel'
 and t_transaction_history_scheduler.end_execution_date is not NULL
