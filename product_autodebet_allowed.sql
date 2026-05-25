3367ed72bd516163d0b32ad9d2c8aa9a0f1049e0
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-05-25 10:41:28 +0700

    Update product_autodebet_allowed.sql

--- product_autodebet_allowed.sql
+++ product_autodebet_allowed.sql
@@ -1,8 +1,12 @@
-SELECT distinct 
-t_transaction_history_scheduler.payee_code,
-master_biller_product.autodebet_allowed 
-FROM t_transaction_history_scheduler
-JOIN
-(select distinct payee_code,autodebet_allowed 
-from master_biller_product)master_biller_product
-on t_transaction_history_scheduler.payee_code =master_biller_product.payee_code
+-- SELECT distinct 
+-- t_transaction_history_scheduler.payee_code,
+-- master_biller_product.autodebet_allowed 
+-- FROM t_transaction_history_scheduler
+-- JOIN
+-- (select distinct payee_code,autodebet_allowed 
+-- from master_biller_product)master_biller_product
+-- on t_transaction_history_scheduler.payee_code =master_biller_product.payee_code
+
+
+select distinct autodebet_allowed
+from master_biller_product
