1f0d9dd55cf5b972889581a555d23eb45bd85883
ninezik <30277780+Ninezik@users.noreply.github.com>
2026-05-25 10:34:16 +0700

    Update product_autodebet_allowed.sql

--- product_autodebet_allowed.sql
+++ product_autodebet_allowed.sql
@@ -2,8 +2,7 @@ SELECT distinct
 t_transaction_history_scheduler.payee_code,
 master_biller_product.autodebet_allowed 
 FROM t_transaction_history_scheduler
-left JOIN
+JOIN
 (select distinct payee_code,autodebet_allowed 
 from master_biller_product)master_biller_product
 on t_transaction_history_scheduler.payee_code =master_biller_product.payee_code
-where t_transaction_history_scheduler.payee_code is not NULL
